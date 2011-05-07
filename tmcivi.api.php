0769f2980b8e0d307f9a711f49dac872d9b69e35
Allen Shaw <allen@nswebsolutions.com>
2011-05-07 02:39:52 -0500

    Adding documentation to hook examples.

--- tmcivi.api.php
+++ tmcivi.api.php
@@ -15,7 +15,7 @@ function hook_tmcivi_template_dir(&$template_dir = array()) {
 function hook_tmcivi_plugins_dir(&$plugins_dir = array()) {
 }
 
-function hook_tmcivi_resource_dir($type, &$plugins_dir = array()) {
+function hook_tmcivi_resource_dir($type) {
 }
 
 function hook_tmcivi_registry() {
@@ -35,8 +35,19 @@ function hook_tmcivi_registry() {
     return $registry;
 }
 
+/**
+ * Register paths for include files for primary tmcivi classes
+ *
+ * @return Array Associative array of any of these keys (values should be full system path to directories):
+ *   page_path: directory containing page preprocessor files called by TM_Page
+ *   form_path: directory containing form processor files called by TM_Form
+ *   raw_path: directory containing preprocessor files for raw output called by TM_Raw_Page
+ */
 function hook_tmcivi_register() {
 }
 
+/**
+ * @return String Full system path to directory containing CiviCRM override files (custom PHP directory) and special tmcivi classes
+ */
 function hook_tmcivi_class_dir() {
 }

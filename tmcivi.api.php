3f7093abdb45fb952f1e8e181f9192dfe6577e95
Allen Shaw <allen@nswebsolutions.com>
2011-05-08 14:50:28 -0500

    changing hook_tmcivi_register to hook_tmcivi_path; cleaning up tmcivi.api.php documentation

--- tmcivi.api.php
+++ tmcivi.api.php
@@ -9,15 +9,60 @@
  * @see hook_tokens
  */
 
-function hook_tmcivi_template_dir(&$template_dir = array()) {
+/**
+ * Define a directory containing additional Smarty templates for your module
+ *
+ * @return String Full system path to directory containing templates
+ */
+function hook_tmcivi_template_dir() {
+    return '/full/path/to/template/directory';
 }
 
-function hook_tmcivi_plugins_dir(&$plugins_dir = array()) {
+/**
+ * Define a directory containing additional Smarty plugins for your module
+ *
+ * @return String Full system path to directory containing plugins
+ */
+function hook_tmcivi_plugins_dir() {
+    return '/full/path/to/plugins/directory';
 }
 
+/**
+ * Define directories containing CSS and JS files for your module
+ *
+ * @param String $type The type of directory requested.
+ * @return Array Drupal path (relative to Drupal base_url) to the directory
+ *    requested; for the following values of $type, return the proper path:
+ *    'css' - directory containing CSS files to be auto-included by file name.
+ *            e.g., The files named x.css in this directory will be automatically
+ *            included when tmref=x
+ *    'js'  - directory containing JS files to be auto-included by file name.
+ *            e.g., The files named x.js in this directory will be automatically
+ *            included when tmref=x
+ *    'base'- directory containing any CSS, JS, image, and other files that may be
+ *            referenced in template output.
+ *
+ *
+ */
 function hook_tmcivi_resource_dir($type) {
+    if ($type == 'js') {
+        return drupal_get_path('module', 'tmcivi_example') . '/resource/js/auto';
+
+    } elseif ($type == 'css') {
+      return drupal_get_path('module', 'tmcivi_example') . '/resource/css/auto';
+
+    } elseif ($type == 'base') {
+      return drupal_get_path('module', 'tmcivi_example') . '/resource';
+
+    }
+
 }
 
+/**
+ * Define any path IDs for this module.
+ *
+ * @return Array
+ */
 function hook_tmcivi_registry() {
     $registry = array(
         /* Format: ID => array(
@@ -38,16 +83,21 @@ function hook_tmcivi_registry() {
 /**
  * Register paths for include files for primary tmcivi classes
  *
+ * The following defaults are used for any paths not defined by hook implementations:
+ *  page_path: [/system/path/to/module/]class/TM/Page
+ *  form_path: [/system/path/to/module/]class/TM/Form
+ *  raw_path : [/system/path/to/module/]class/TM/Raw
+ *
  * @return Array Associative array of any of these keys (values should be full system path to directories):
- *   page_path: directory containing page preprocessor files called by TM_Page
- *   form_path: directory containing form processor files called by TM_Form
- *   raw_path: directory containing preprocessor files for raw output called by TM_Raw_Page
+ *   page: directory containing page preprocessor files called by TM_Page
+ *   form: directory containing form processor files called by TM_Form
+ *   raw: directory containing preprocessor files for raw output called by TM_Raw_Page
  */
-function hook_tmcivi_register() {
+function hook_tmcivi_path() {
     $return = array(
-        'page_path' => '',
-        'form_path' => '',
-        'raw_path'  => '',
+        'page' => '',
+        'form' => '',
+        'raw'  => '',
     );
     return $return;
 }
@@ -56,4 +106,5 @@ function hook_tmcivi_register() {
  * @return String Full system path to directory containing CiviCRM override files (custom PHP directory) and special tmcivi classes
  */
 function hook_tmcivi_class_dir() {
+    return '/full/path/to/custom/PHP/directory';
 }
